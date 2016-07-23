702b5ab175c0904f2fa727485957cca33ce4bb03
Tim Holy <tim.holy@gmail.com>
2016-07-23 11:20:32 -0500

    Initial working commit

--- src/ImagesAxes.jl
+++ src/ImagesAxes.jl
@@ -1,5 +1,48 @@
 module ImagesAxes
 
-# package code goes here
+using Reexport
+
+@reexport using AxisArrays
+# @reexport using Traitor
+
+export @timeaxis, timeaxis, HasTimeAxis
+
+abstract AxisType
+immutable TimeAxis <: AxisType end
+immutable SpaceAxis <: AxisType end
+
+# By default, axes are spatial
+AxisType{T<:Axis}(::Type{T}) = SpaceAxis
+
+macro timeaxis(T)
+    :(AxisType{T<:$T}(::Type{T}) = TimeAxis)
+end
+
+"""
+    @timeaxis Axis{:axname}
+
+declares that any `Axis` with name `:axname` is a temporal axis. This
+declaration must be made before you call functions on such arrays.
+"""
+:@timeaxis
+
+"""
+    timeaxis(A)
+
+returns the time axis, if present, of the array `A`, and `nothing` otherwise.
+"""
+@inline timeaxis(A::AxisArray) = _timeaxis(A.axes...)
+# A prettier implementation using Traitor:
+# @inline _timeaxis(ax, axes...) = _timeaxis(axes...)
+# @traitor _timeaxis(ax::Axis::TimeAxis, axes...) = ax
+@inline _timeaxis(ax, axes...) = __timeaxis(AxisType(typeof(ax)), ax, axes...)
+@inline __timeaxis(::Type{TimeAxis}, ax, axes...) = ax
+@inline __timeaxis(::Type,           ax, axes...) = _timeaxis(axes...)
+_timeaxis() = nothing
+
+abstract AbstractHasTimeAxis
+immutable HasTimeAxis <: AbstractHasTimeAxis end
+
+Base.@pure AbstractHasTimeAxis{T,N,D,Ax}(::Type{AxisArray{T,N,D,Ax}}) = any(S->S<:TimeAxis, Ax.parameters)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
-using ImagesAxes
-using Base.Test
+using ImagesAxes, SIUnits.ShortUnits, Base.Test
 
-# write your own tests here
-@test 1 == 1
+@timeaxis Axis{:time}
+
+# using Traitor
+# has_time_axis(A) = false
+# @traitor has_time_axis(A::AxisArray::HasTimeAxis) = true
+has_time_axis(A) = _has_time_axis(timeaxis(A), A)
+_has_time_axis(::Void, ::Any) = false
+_has_time_axis(::Axis, ::Any) = true
+
+A = AxisArray(reshape(1:12, 3, 4), Axis{:x}(1:3), Axis{:y}(1:4))
+@test timeaxis(A) == nothing
+@test has_time_axis(A) == false
+axt = Axis{:time}(1s:1s:4s)
+A = AxisArray(reshape(1:12, 3, 4), Axis{:x}(1:3), axt)
+@test timeaxis(A) === axt
+@test has_time_axis(A) == true
